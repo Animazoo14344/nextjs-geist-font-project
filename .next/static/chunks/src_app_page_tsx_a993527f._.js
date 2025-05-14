ed0c9278a7099504ac955f6eae4ab2811995a781
Animazoo14344 <john.yian@gennexcorp.com>
2025-05-15 00:59:43 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747241983007

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7ef6f416._.js",
+  "static/chunks/node_modules_ef0b1be8._.js"
+],
+    source: "dynamic"
+});
